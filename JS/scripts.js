946d23136c96d4692df04e96d3d6324670c6d8fc
Emmanuel Guedes <emmanuel_guedes@hotmail.com>
2022-12-17 14:25:17 -0300

    iniciando o javascript

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,1 +1,30 @@
-console.log("teste");
+//Elementos
+const multiplicationForm = document.querySelector("multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiplicationInput = document.querySelector("#multiplicatior");
+const multiplicationTitle = document.querySelector("#multiplication-title span");
+const multiplicationTable = document.querySelector("#multiplication-operations");
+
+//Funções
+
+const createTable = (number, multiplicatorNumber) => {
+    multiplicationTable.innerHTML = "";
+
+    for (i = 1; i<= multiplicatorNumber; i++) {
+        const result = number *1;
+
+        const template = `<div class= "row">
+        <div class = "operations">${number} x ${i} = </div>
+        <div class = "result">${result}</div>
+        </div>`;
+
+        const parser = new DOMParser();
+        const htmlTemplate = parser.parseFromString(template, "text/html");
+        const row = htmlTemplate.querySelector(".row");
+
+        multiplicationTable.appendChild(row);
+
+    }
+    multiplicationTitle.innerText = number;
+
+};
